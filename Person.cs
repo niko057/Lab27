baa5c0888f3ceb1a7774657d90e63b379578ddd5
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-24 12:56:47 +0400

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab27
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set;}
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace Lab27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ArrayList arrayList=new ArrayList();
+
+            Person person=new Person();
+
+            person.Name = "Nicat";
+            person.Age = 27;
+            arrayList.Add(person);
+
+            Person person1 = new Person();
+
+            person1.Name = "Ali";
+            person1.Age = 23;
+            arrayList.Add(person1);
+
+            Person person2 = new Person();
+
+            person2.Name = "Aziz";
+            person2.Age = 24;
+            arrayList.Add(person2);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+
+        }
+    }
+}
