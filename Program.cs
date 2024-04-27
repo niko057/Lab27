5025e47cde622c0005e2034ff6fc7348dfee3c0e
niko057 <nicatnamazov57.nn@gmail.com>
2024-04-28 00:24:30 +0400

    second

--- Program.cs
+++ Program.cs
@@ -6,30 +6,60 @@ namespace Lab27
     {
         static void Main(string[] args)
         {
-           ArrayList arrayList=new ArrayList();
+            //ArrayList arrayList=new ArrayList();
 
-            Person person=new Person();
+            // Person person=new Person();
 
-            person.Name = "Nicat";
-            person.Age = 27;
-            arrayList.Add(person);
+            // person.Name = "Nicat";
+            // person.Age = 27;
+            // arrayList.Add(person);
 
-            Person person1 = new Person();
+            // Person person1 = new Person();
 
-            person1.Name = "Ali";
-            person1.Age = 23;
-            arrayList.Add(person1);
+            // person1.Name = "Ali";
+            // person1.Age = 23;
+            // arrayList.Add(person1);
 
-            Person person2 = new Person();
+            // Person person2 = new Person();
 
-            person2.Name = "Aziz";
-            person2.Age = 24;
-            arrayList.Add(person2);
+            // person2.Name = "Aziz";
+            // person2.Age = 24;
+            // arrayList.Add(person2);
 
-            foreach (var item in arrayList)
+            // foreach (var item in arrayList)
+            // {
+            //     Console.WriteLine(item.ToString());
+            // }
+
+
+            string[] arr = { "Nicat", "Ali", "Aziz", "Pasa" };
+
+            Console.WriteLine("Daxil edin ");
+
+            char input = Convert.ToChar(Console.ReadLine());
+
+            for(int i = 0;i < arr.Length; i++)
             {
-                Console.WriteLine(item.ToString());
+                for (int j = 0; j <arr[i].Length; j++)
+                {
+                    
+                    if (arr[i][j] == input)
+                    {
+                        Console.WriteLine(arr[i] + " " + j);
+                    }
+                  
+                }
             }
+           
+
+
+
+
+
+
+
+
+
 
 
         }
